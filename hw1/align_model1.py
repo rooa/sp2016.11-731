36293e00833f382152aebc32b4da0b465c595a9e
rooa <hiroakihayash88@gmail.com>
2016-01-29 22:35:57 -0500

    Simplify initialization

--- hw1/align_model1.py
+++ hw1/align_model1.py
@@ -33,9 +33,7 @@ p_e_given_f = defaultdict(float)
 e_vocab = len(e_count.keys())
 
 for n, f_i in enumerate(f_count.keys()):
-    marginal = filter(lambda (x, y): x == f_i, fe_count.keys())
-    for (f, e) in marginal:
-        p_e_given_f[(f, e)] = 1.0 / (e_vocab + 1)  # NULL?
+    p_e_given_f.update({(f, e): 1.0 / (e_vocab + 1) for (f, e) in filter(lambda (x, y): x == f_i, fe_count.keys())})  # NULL?
     # p_e_given_f.update({(f, e): 1.0 / (m + 1) for (f, e) in marginal})
     if n % 100 == 0:
         sys.stderr.write(".")
